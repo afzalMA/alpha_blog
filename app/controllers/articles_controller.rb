c85df15975374203ff7ec46100a6b6de6ff0b120
afzalMa <afzal.m@etvbharat.com>
2020-06-12 13:40:39 +0530

    add ability to create new articles from front-end

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,15 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def new
+        
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        #render plain: @article.inspect
+        @article.save
+        redirect_to @article
+    end
 end
