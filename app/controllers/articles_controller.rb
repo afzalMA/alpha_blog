fc11489aa82bd5088b6eebb4307b07bdef2a5766
afzalMa <afzal.m@etvbharat.com>
2020-07-17 17:17:40 +0530

    controller methods as helper methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ before_action :set_article, only: [:show, :edit, :update, :destroy]
 
     def create
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         #render plain: @article.inspect
         if @article.save
             flash[:notice] = "Article was created successfully"
