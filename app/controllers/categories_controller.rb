2c77a0bfd15f4cc09c350faf20a0fd0ccad1429a
afzalMa <afzal.m@etvbharat.com>
2020-08-03 16:42:43 +0530

    update article views to display categories and complete category index

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,6 +7,8 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5).order(updated_at: :desc)
+
   end
 
   def new
